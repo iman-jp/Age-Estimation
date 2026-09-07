1b9a65a5ac16a78d1abf9b543737bd36d1b860a3
Iman <imanjahanpanah121@gamil.com>
2026-09-07 23:20:34 +0200

    adding a fallback function to the segmentation.py

--- tests/segmentation.py
+++ tests/segmentation.py
@@ -4,10 +4,68 @@ import mediapipe as mp
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw
+import cv2
 
 
 
 
+def fall_back(pose_result, face_landmarker, image_np, w: int, h: int):
+    if not pose_result or not pose_result.pose_landmarks:
+        return None
+
+    pose_landmarks = pose_result.pose_landmarks[0]
+            
+    #key facial landmarks in MediaPipe Pose
+    head_indices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    head_xs = [pose_landmarks[i].x * w for i in head_indices if pose_landmarks[i].visibility > 0.3]
+    head_ys = [pose_landmarks[i].y * h for i in head_indices if pose_landmarks[i].visibility > 0.3]
+
+    if not head_xs or not head_ys:
+        return None
+
+    center_x = (min(head_xs) + max(head_xs)) / 2
+    center_y = (min(head_ys) + max(head_ys)) / 2
+
+    raw_width = max(head_xs) - min(head_xs)
+    raw_height = max(head_ys) - min(head_ys)
+    
+    
+    side_length = max(raw_width, raw_height) * 1.4
+
+    crop_min_x = max(0, int(center_x - side_length / 2))
+    crop_max_x = min(w, int(center_x + side_length / 2))
+    crop_min_y = max(0, int(center_y - side_length / 2))
+    crop_max_y = min(h, int(center_y + side_length / 2))
+
+    
+    cropped_np = image_np[crop_min_y:crop_max_y, crop_min_x:crop_max_x]
+
+    if cropped_np.size > 0:
+    
+        target_size = 512  # Ideal size for MediaPipe face detection
+        crop_h, crop_w = cropped_np.shape[:2]
+
+        if crop_h < target_size or crop_w < target_size:
+            cropped_np = cv2.resize(
+                cropped_np, 
+                (target_size, target_size), 
+                interpolation=cv2.INTER_CUBIC
+            )
+
+        
+        #Image.fromarray(cropped_np).show()
+
+        
+        cropped_mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=np.ascontiguousarray(cropped_np))
+        
+        face_result = face_landmarker.detect(cropped_mp_image)
+        
+        if face_result and face_result.face_landmarks:
+            print("Face successfully detected in cropped head region!")
+            return (face_result,cropped_np)
+
+    return None
+ 
 
 
 def get_region_mask(indices:list,w:int,h:int,landmarks):
@@ -16,6 +74,7 @@ def get_region_mask(indices:list,w:int,h:int,landmarks):
     :param indices : the section we want to extract as a list of num
     :param w : wide of the matplot
     :param h : height of the matplot
+    :param landmarks : all the point made by mediapipe
     :return : array of the given segment
     """
     mask = Image.new('L', (w, h), 0)
@@ -43,35 +102,41 @@ def extract_face_regions(image_input):
     mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image_np)
     face_result = face_landmarker.detect(mp_image)
 
-    if not face_result.face_landmarks:
-        print("No face detected")
-        return
+    if not face_result or not face_result.face_landmarks:
+        print("Direct face detection failed. Triggering pose fallback...")
+        pose_result = pose_landmarker.detect(mp_image)
+    
+        face_result,image_np = fall_back(pose_result, face_landmarker, image_np, w, h)
+        h, w = image_np.shape[:2]
+
+    if not face_result or not face_result.face_landmarks:
+        print("No face detected after fallback attempts.")
+        return None
+        
+        
 
     landmarks = face_result.face_landmarks[0]
-
-    
-
     
     
-    # left_eye_mask   = get_region_mask(BodyPartMask.LEFT_EYE.value,w,h,landmarks)
-    # right_eye_mask  = get_region_mask(BodyPartMask.RIGHT_EYE.value,w,h,landmarks)
+    left_eye_mask   = get_region_mask(BodyPartMask.LEFT_EYE.value,w,h,landmarks)
+    right_eye_mask  = get_region_mask(BodyPartMask.RIGHT_EYE.value,w,h,landmarks)
     nose_mask       = get_region_mask(BodyPartMask.NOSE.value,w,h,landmarks)
-    lips_mask       = get_region_mask(BodyPartMask.LIPS.value,w,h,landmarks)
-    chin_mask       = get_region_mask(BodyPartMask.CHIN.value,w,h,landmarks)
+    lips_mask       = get_region_mask(BodyPartMask.EYES_E.value,w,h,landmarks)
+    chin_mask       = get_region_mask(BodyPartMask.LOWER_FACE.value,w,h,landmarks)
     eyes_mask       = get_region_mask(BodyPartMask.UPPER_FACE.value,w,h,landmarks)
 
-    # left_eye_img    = apply_mask(image_np, left_eye_mask)
-    # right_eye_img   = apply_mask(image_np, right_eye_mask)
+    left_eye_img    = apply_mask(image_np, left_eye_mask)
+    right_eye_img   = apply_mask(image_np, right_eye_mask)
     nose_img        = apply_mask(image_np, nose_mask)
     lips_img        = apply_mask(image_np, lips_mask)
     chin_img        = apply_mask(image_np,chin_mask)
     eyses_img       = apply_mask(image_np,eyes_mask)
 
     
-    fig, axes = plt.subplots(1, 6, figsize=(18, 4))
-    titles = ['Original', 'Left Eye', 'Right Eye', 'Nose', 'Lips','Chin']
-    # images = [image_np, left_eye_img, right_eye_img, nose_img, lips_img, chin_img]
-    images = [image_np, eyses_img, nose_img, lips_img, chin_img]
+    fig, axes = plt.subplots(1, 7, figsize=(18, 4))
+    titles = ['Original', 'Left Eye', 'Right Eye', 'Nose', 'Lips','Chin','eyes']
+    images = [image_np, left_eye_img, right_eye_img, nose_img, lips_img, chin_img, eyses_img]
+    #images = [image_np, eyses_img, nose_img, lips_img, chin_img]
 
     for ax, img, title in zip(axes, images, titles):
         ax.imshow(img)
@@ -82,9 +147,9 @@ def extract_face_regions(image_input):
     plt.show()
 
 # extract_face_regions(r'..\utkface_images\105_1_0_20170112213507183.jpg')
-# extract_face_regions(r'C:\Users\imanj\Desktop\Age-Estimation\utkface_images\26_0_0_20170117144510833.jpg')
-# extract_face_regions(r'C:\Users\imanj\Desktop\Age-Estimation\utkface_images\27_1_0_20170117014038281.jpg')
-# extract_face_regions(r'C:\Users\imanj\Desktop\Age-Estimation\utkface_images\25_0_0_20170113210127389.jpg')
-# extract_face_regions(r'C:\Users\imanj\Desktop\Age-Estimation\utkface_images\21_1_4_20170103224438000.jpg')
+extract_face_regions(r"C:\Users\imanj\Desktop\Age-Estimation\data\utkface_images\25_1_0_20170104021710995.jpg")
+# extract_face_regions(r"C:\Users\imanj\Pictures\Screenshots\Screenshot 2026-09-07 195310.png")
+# extract_face_regions(r"C:\Users\imanj\Pictures\Camera Roll\WIN_20260903_14_02_38_Pro.jpg")
+# extract_face_regions(r"C:\Users\imanj\Pictures\Camera Roll\WIN_20260903_14_02_43_Pro.jpg")
 # extract_face_regions(r'C:\Users\imanj\Desktop\Age-Estimation\utkface_images\21_1_1_20170116214444631.jpg')
 # extract_face_regions(r'C:\Users\imanj\Desktop\Age-Estimation\utkface_images\21_0_1_20170116030053264.jpg')
